c5388aa8cf1d5aa0431b0ad650ce5ca4931bf47b
MSzostaq <maciek.szostak55@gmail.com>
2021-01-12 10:06:38 +0100

    components/button

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: #d3d3d3;
+  border-radius: 24px;
+  font-family: sans-serif;
+  font-size: 24px;
+  margin: 8px;
+  padding: 8px;
+  width: 240px;
+  height: 48px;
+`;
+
+export default Button;
