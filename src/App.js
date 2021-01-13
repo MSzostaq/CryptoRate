ff32937cf0c2d6245eb6cf1da3ae0c72d634d0fd
MSzostaq <maciek.szostak55@gmail.com>
2021-01-13 08:27:27 +0100

    ToggleButton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+// import styled from "styled-components";
 import Button from "components/Button";
+import Toggle from "components/Toggle";
 
-const App = () => (
-  <div>
-    <h1>Hello</h1>
-    <Button>ok</Button>
-  </div>
-);
+function App() {
+  const [value, setValue] = useState(true);
+
+  function onValueChange(newValue) {
+    setValue(newValue);
+  }
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Button>ok</Button>
+      <Toggle value={value} onChange={onValueChange} />
+    </div>
+  );
+}
 
 export default App;
