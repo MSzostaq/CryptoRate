281f70d53893ff856085bb768a1b182e7fd77ff4
MSzostaq <maciek.szostak55@gmail.com>
2021-01-14 14:15:49 +0100

    sidebar buttons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,12 +7,22 @@ import trophyIcon from "icons/trophyIcon.svg";
 import profitsIcon from "icons/profitsIcon.svg";
 import walletIcon from "icons/walletIcon.svg";
 
-const StyledButtonIcon = styled(ButtonIcon)`
-  display: block;
-  padding: 10px 10px 10px 18px;
+const StyledDiv = styled.div`
+  align-items: center;
+  border-radius: 4px;
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 24px;
+  width: 180px;
+  height: 60px;
   &:hover {
     background-color: rgba(0, 0, 0, 0.1);
   }
+`;
+
+const StyledButtonIcon = styled(ButtonIcon)`
+  display: block;
+  padding: 10px 10px 10px 18px;
 `;
 
 const StyledWrapper = styled.nav`
@@ -31,9 +41,18 @@ const StyledWrapper = styled.nav`
 function Sidebar() {
   return (
     <StyledWrapper>
-      <StyledButtonIcon icon={trophyIcon} />
-      <StyledButtonIcon icon={walletIcon} />
-      <StyledButtonIcon icon={profitsIcon} />
+      <StyledDiv>
+        <StyledButtonIcon icon={trophyIcon} />
+        <p>Dashboard</p>
+      </StyledDiv>
+      <StyledDiv>
+        <StyledButtonIcon icon={walletIcon} />
+        <p>My Wallet</p>
+      </StyledDiv>
+      <StyledDiv>
+        <StyledButtonIcon icon={profitsIcon} />
+        <p>Graphs</p>
+      </StyledDiv>
     </StyledWrapper>
   );
 }
