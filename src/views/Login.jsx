a9c9f9f5e24e1ac7c935736b9fd88d3f380f3683
MSzostaq <maciek.szostak55@gmail.com>
2021-01-29 09:56:24 +0100

    login page

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -5,19 +5,41 @@ import Button from "components/Button";
 const View = styled.div`
   background-color: #f8f8f8;
   display: grid;
+  width: 100%;
+  height: 100%;
+`;
+
+const StyledButton = styled(Button)`
+  margin: 12px;
+`;
+
+const StyledWrapper = styled.div`
+  position: fixed;
+  top: 240px;
+  left: 24px;
+  width: 320px;
+  height: 320px;
 `;
 
 const StyledHeading = styled.h1`
   font-size: 64px;
   font-weight: bold;
+  position: absolute;
   height: 120px;
 `;
 
 function Login() {
   return (
     <View>
-      <StyledHeading />
-      <Button>Login</Button>
+      <StyledHeading>
+        <h1>CryptoRate</h1>
+      </StyledHeading>
+      <StyledWrapper>
+        <StyledButton>Enter Your Email</StyledButton>
+        <StyledButton>Enter Your Password</StyledButton>
+        <StyledButton>Login</StyledButton>
+        <StyledButton>Create Account</StyledButton>
+      </StyledWrapper>
     </View>
   );
 }
